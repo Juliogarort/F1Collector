3890d04425fc7f41e0e4d5541af36088c031ca26
AlbertoBernal02 <abernalmejias@hotmail.com>
2025-03-22 14:03:48 +0100

    Creación de proyecto laravel usando dependecias como vue, node, middleware, fortify, spatie

---
+++ F1Collector/resources/js/app.js
@@ -0,0 +1,9 @@
+import './bootstrap';
+import { createApp } from 'vue';
+
+const app = createApp({});
+
+// Componentes Vue
+// Ej: app.component('example-component', require('./components/ExampleComponent.vue').default);
+
+app.mount('#app');
