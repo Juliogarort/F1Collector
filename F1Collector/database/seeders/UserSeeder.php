e6a05a0758330b0ee3f6c74c3c9fb5342de50dc7
Julio García <julio.garort16@gmail.com>
2025-05-20 18:49:52 +0200

    🔧 Añadir nuevos usuarios al seeder: se incluyen los usuarios 'Paco', 'Julio' y 'Alberto' con sus respectivos correos y contraseñas.

--- F1Collector/database/seeders/UserSeeder.php
+++ F1Collector/database/seeders/UserSeeder.php
@@ -26,6 +26,30 @@ class UserSeeder extends Seeder
                 'user_type' => 'Customer',
                 'created_at' => now(),
                 'updated_at' => now(),
+            ],
+            [
+                'name' => 'Paco',
+                'email' => 'paco@example.com',
+                'password' => Hash::make('paco1234'),
+                'user_type' => 'Customer',
+                'created_at' => now(),
+                'updated_at' => now(), 
+            ],
+            [
+                'name' => 'Julio',
+                'email' => 'julio@example.com',
+                'password' => Hash::make('julio1234'),
+                'user_type' => 'Admin',
+                'created_at' => now(),
+                'updated_at' => now(), 
+            ],
+            [
+                'name' => 'Alberto',
+                'email' => 'alberto@example.com',
+                'password' => Hash::make('alberto1234'),
+                'user_type' => 'Admin',
+                'created_at' => now(),
+                'updated_at' => now(), 
             ]
         ]);
     }
